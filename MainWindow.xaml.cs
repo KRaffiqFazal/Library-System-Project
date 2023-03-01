dc5acd49246288d00110d8cb59727fa41ce4ab79
Kazim Raffiq-Fazal <KR721@student.aru.ac.uk>
2023-03-01 21:00:31 +0000

    Added new windows with functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +14,152 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Library_System
+namespace Library_System //I think we need to find the Window class and change it to have a storage for global values with a global object that contains all variables, then use that new Window as the partial class location thing for the other stuff.
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    public class Globals
+    {
+        //usage in Window1.xaml
+        public String userID;
+    }
     public partial class MainWindow : Window
     {
+        
+        //usage in MainWindow.xaml
+        Stopwatch stopwatch;
+        int attempts;
+        String terminalPassword = "Admin123";
+
         
         public MainWindow()
         {
             InitializeComponent();
+            Globals globalValues = new Globals();
+            globalValues.userID = null;
+            Hide2();
+        }
+
+        private void Hide1()
+        {
+            btnProceed.Visibility = Visibility.Hidden;
+            btnQuit.Visibility = Visibility.Hidden;
+            picLogo.Visibility = Visibility.Hidden;
+        }
+        private void Hide2() 
+        {
+            btnAdminQuit.Visibility = Visibility.Hidden;
+            btnRestart.Visibility = Visibility.Hidden;
+            txtbxWatermarkedText.Visibility = Visibility.Hidden;
+            pswdbxTerminalPassword.Visibility = Visibility.Hidden;
+            lblUserHelp.Visibility = Visibility.Hidden;
+            attempts = 3;
+        }
+        private void Reveal1()
+        {
+            btnProceed.Visibility = Visibility.Visible;
+            btnQuit.Visibility = Visibility.Visible;
+            picLogo.Visibility = Visibility.Visible;
+        }
+        private void Reveal2() 
+        {
+            btnAdminQuit.Visibility = Visibility.Visible;
+            btnRestart.Visibility = Visibility.Visible;
+            txtbxWatermarkedText.Visibility = Visibility.Visible;
+            pswdbxTerminalPassword.Visibility = Visibility.Visible;
+            lblUserHelp.Visibility = Visibility.Visible;
+        }
+        private void btnProceed_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1();
+            win2.Show();
+            Close();
+
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/questions/26682729/event-for-when-a-button-is-held-clicked-for-some-time
+        /// </summary>
+        private void btnQuit_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+        }
+
+        private void btnQuit_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            stopwatch.Stop();
+            if (stopwatch.ElapsedMilliseconds >= 2000) //Admin quit function change to 5000
+            {
+                Hide1();
+                Reveal2();
+                
+            }
+        }
+
+        /// <summary>
+        /// https://www.youtube.com/watch?v=YPwnBJod5a8
+        /// </summary>
+        private void txtbxWatermarkedText_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtbxWatermarkedText.Visibility = Visibility.Collapsed;
+            pswdbxTerminalPassword.Visibility = Visibility.Visible;
+            pswdbxTerminalPassword.Focus();
+        }
+
+        private void pswdbxTerminalPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(pswdbxTerminalPassword.Password))
+            {
+                pswdbxTerminalPassword.Visibility = Visibility.Collapsed;
+                txtbxWatermarkedText.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Restart()
+        {
+            Hide2();
+            Reveal1();
+        }
+        /// <summary>
+        /// https://learn.microsoft.com/en-us/dotnet/api/system.windows.input.keyboard.keydown?view=windowsdesktop-7.0
+        /// </summary>
+        private void PasswordEnter()
+        {
+                if (pswdbxTerminalPassword.Password.Equals(terminalPassword))
+                {
+                    Application.Current.Shutdown();
+                }
+                else
+                {
+                    attempts--;
+                    if (attempts <= 0)
+                    {
+                        Restart();
+                    }
+                    pswdbxTerminalPassword.Password = null;
+                }
+        }
+        private void btnAdminQuit_Click(object sender, RoutedEventArgs e)
+        {
+            PasswordEnter();
+        }
+
+        private void btnRestart_Click(object sender, RoutedEventArgs e)
+        {
+            Restart();
+        }
+
+        /// <summary>
+        /// https://learn.microsoft.com/en-us/dotnet/api/system.windows.input.keyboard.keydown?view=windowsdesktop-7.0
+        /// </summary>
+        private void pswdbxTerminalPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key== Key.Enter) 
+            {
+                PasswordEnter();
+            }
         }
     }
 }
---
+++ Window1.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library_System
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            txtblkLoginError.Visibility = Visibility.Hidden;
+            txtblkLoginErrorOmission.Visibility = Visibility.Hidden;
+        }
+
+        private List<String> UpdateUserIDs()
+        {
+            //will look through xml file for all user IDs that exist and make a list of them
+            List<String> list = new List<String> {"Admin123"};
+            return list;
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (pswdbxPassword.Password.Equals(""))
+            {
+                System.Media.SystemSounds.Beep.Play();
+                txtblkLoginErrorOmission.Visibility = Visibility.Visible;
+            }
+            else if (UpdateUserIDs().Contains(pswdbxPassword.Password))
+            { 
+                globalValues.userID = pswdbxPassword.Password;
+            }
+        }
+
+        private void txtbxWatermarkedText_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtbxWatermarkedText.Visibility = Visibility.Collapsed;
+            pswdbxPassword.Visibility = Visibility.Visible;
+            pswdbxPassword.Focus();
+        }
+
+        private void pswdbxTerminalPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(pswdbxPassword.Password))
+            {
+                pswdbxPassword.Visibility = Visibility.Collapsed;
+                txtbxWatermarkedText.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
